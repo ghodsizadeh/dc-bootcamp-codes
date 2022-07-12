9f3df8bce3142cefba3bef8b2199c1a68ce9e985
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2022-07-12 19:34:20 +0430

    feat: add query lambda function

---
+++ airbnb_project/lambda_functions/tests/test_query.py
@@ -0,0 +1,87 @@
+"""
+Test functionality of query lambda functions with following tests
+1. Health check and testing with no query parameters
+2. Testing with query parameters with area
+3. Testing with query parameters with check in and check out
+4. Testing with query parameters with check in and check out with no rooms
+"""
+# pylint: disable=unused-argument
+
+from query import lambda_handler
+
+
+def test_query_base(listings_collection_one_room, sample_room):
+    """
+    Test if function works with one room in the database
+    """
+    event = {}
+    context = {}
+    response = lambda_handler(event, context)
+    assert response["statusCode"] == 200
+    body = response["body"]
+    assert body == [sample_room]
+
+
+def test_query_with_area(listings_collection, sample_rooms):
+    """Test query with area"""
+    room = sample_rooms[0]
+    area = room["neighborhood"]
+    event = {
+        "queryStringParameters": {
+            "area": area,
+            # "check_in": "2020-01-01",
+            # "check_out": "2020-01-02",
+        }
+    }
+    context = {}
+    response = lambda_handler(event, context)
+    expected_rooms = [room for room in sample_rooms if room["neighborhood"] == area]
+    body = response["body"]
+
+    assert response["statusCode"] == 200
+    assert len(body) == len(expected_rooms)
+    assert len(body) != len(sample_rooms)
+
+
+def test_query_with_check_in_check_out(listings_collection, sample_rooms):
+    """Test query with check in and check out"""
+    room = sample_rooms[0]
+    check_in = room["calendar"][0]["date"]
+    check_out = room["calendar"][-1]["date"]
+    event = {
+        "queryStringParameters": {
+            "check_in": check_in,
+            "check_out": check_out,
+        }
+    }
+    context = {}
+    response = lambda_handler(event, context)
+    expected_rooms = [
+        room
+        for room in sample_rooms
+        if check_in <= room["calendar"][0]["date"] <= check_out
+    ]
+    body = response["body"]
+
+    assert response["statusCode"] == 200
+    assert len(body) == len(expected_rooms)
+
+
+def test_query_with_check_in_check_out_empty(listings_collection, sample_rooms):
+    """Test query with check in and check out"""
+    room = sample_rooms[0]
+    check_in = room["calendar"][11]["date"]
+    check_out = room["calendar"][18]["date"]
+    event = {
+        "queryStringParameters": {
+            "check_in": check_in,
+            "check_out": check_out,
+        }
+    }
+    context = {}
+    response = lambda_handler(event, context)
+
+    body = response["body"]
+
+    assert response["statusCode"] == 200
+    assert len(body) == 0
