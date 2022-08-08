ffeca1c865a012e95e1fbfe13dca2264c2d8b51e
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2022-08-08 20:53:07 +0430

    feat: update infra with new vpc config

--- airbnb_project/infra/var.tf
+++ airbnb_project/infra/var.tf
@@ -11,6 +11,13 @@ variable "lambda_query_zip" {
   default = "../lambda_functions/query.zip"
 }
 
+variable "lambda_booking_zip" {
+  default = "../lambda_functions/booking.zip"
+}
+variable "lambda_update_db_zip" {
+  default = "../lambda_functions/update_db.zip"
+}
+
 variable "lambda_layers_file" {
   default = "../lambda_functions/package.zip"
 }
